7ca48881b321014779f9fcaef8e0c230f4443803
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-01-04 20:30:42 +0100

    init code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Typography>Hello world</Typography>
+    </React.Fragment>
+  );
+}
+
+export default App;
